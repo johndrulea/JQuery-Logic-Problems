6d7f1a3e9f3b75096ea75afa144244528becd8d1
johndrulea <59680729+johndrulea@users.noreply.github.com>
2020-04-01 23:35:25 -0700

    Update mathy.js

--- mathy.js
+++ mathy.js
@@ -11,6 +11,12 @@ module.exports = {
             }
             return num2;
         },
+        less: function (num1, num2){
+            if(num1 < num2){
+                return num1;
+            }
+            return num2;
+        },
         isEven: function(num){
             return num % 2 === 0;
         },
